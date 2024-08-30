a43b67a34584f99df13ae84569c20f2b67668880
Dean Minnie <dminnie33@gmail.com>
2024-08-30 15:39:19 +0200

    Update main.c

--- UI/main.c
+++ UI/main.c
@@ -1,34 +1,68 @@
 #include "raylib.h"
-
+#include <math.h>
 #include <stdbool.h>
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 450
-#define OLED_WIDTH 128
-#define OLED_HEIGHT 64
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define PIXEL_SIZE 5
 
+bool pix_map[SCREEN_WIDTH * SCREEN_HEIGHT];
 
-bool pixMap[OLED_WIDTH * OLED_HEIGHT];
+void reset_Screen() {
+  for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
+    pix_map[i] = false;
+  }
+}
+
+int xyToIndex(int x, int y) { return x + (y * SCREEN_WIDTH); }
 
 int main(void) {
-  InitWindow(SCREEN_HEIGHT, SCREEN_HEIGHT,
-             "raylib [core] example - basic window");
+  Vector2 mousePosition;
+  int prevMouseX = -1;
+  int prevMouseY = -1;
 
-  SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+  reset_Screen();
 
-  while (!WindowShouldClose()) // Detect window close button or ESC key
-  {
+  InitWindow(SCREEN_WIDTH * PIXEL_SIZE, SCREEN_HEIGHT * PIXEL_SIZE, "OLED Draw");
+
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()) {
+    mousePosition = GetMousePosition();
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
-    DrawRectangle(0, 0, GetScreenWidth() / 2, 30, Fade(MAGENTA, 0.6f));
 
-    // DrawText("Congrats! You created your first window!", 190, 200, 20,
-    //          LIGHTGRAY);
+    for (int i = 0; i < SCREEN_WIDTH; i += 1) {
+      for (int j = 0; j < SCREEN_HEIGHT; j += 1) {
+        Color c;
+        if (pix_map[xyToIndex(i, j)]) {
+          c = WHITE;
+        } else {
+          c = BLACK;
+        }
+        DrawRectangle(i * PIXEL_SIZE, j * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE,
+                      c);
+      }
+    }
 
     EndDrawing();
+
+    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+      int mousex = floor(mousePosition.x / PIXEL_SIZE);
+      int mousey = floor(mousePosition.y / PIXEL_SIZE);
+
+      pix_map[xyToIndex(mousex, mousey)] = true;
+
+      prevMouseX = mousex;
+      prevMouseY = mousey;
+    }
+
+    if(IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)){
+      reset_Screen();
+    }
   }
 
-  CloseWindow(); // Close window and OpenGL context
+  CloseWindow();
   return 0;
 }
